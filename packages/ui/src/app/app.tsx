1d530a6f0948bf0f0381937d247a11fdcfc5812d
Romain Tabary <tabaryr@gmail.com>
2022-10-20 18:24:27 +0200

    feat(ui): update routes

--- packages/ui/src/app/app.tsx
+++ packages/ui/src/app/app.tsx
@@ -1,21 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ContentWithSidebar from './layouts/content-with-sidebar/content-with-sidebar';
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
 
 const About = React.lazy(() => import('./pages/about/about'));
 const Home = React.lazy(() => import('./pages/home/home'));
+const NotFound = React.lazy(() => import('./pages/not-found/not-found'));
+const Login = React.lazy(() => import('./layouts/login/login'));
 
 export function App() {
   return (
@@ -38,8 +29,17 @@ export function App() {
             </React.Suspense>
           }
         />
-        <Route path="*" element={<NoMatch />} />
+        <Route
+          path="apis/uuid/details"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Home />
+            </React.Suspense>
+          }
+        />
       </Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
