72097b04ab3f0063da5f65e5e9c01c364a849380
RTabary <tabaryr@gmail.com>
2022-10-21 16:16:21 +0200

    refactor(ui): refactor app bar and sidebar (#5)

---
+++ packages/ui/src/app/components/sidebar/sidebar.tsx
@@ -0,0 +1,99 @@
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { CSSObject, Divider, IconButton, List, Theme } from '@mui/material';
+import MuiDrawer from '@mui/material/Drawer';
+import { styled, useTheme } from '@mui/material/styles';
+import { drawerWidth } from '../../utils/constants';
+import SidebarItem from '../sidebar-item/sidebar-item';
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+/* eslint-disable-next-line */
+export interface SidebarProps {
+  open: boolean;
+  handleDrawerClose: () => void;
+}
+
+export function Sidebar({ open, handleDrawerClose }: SidebarProps) {
+  const theme = useTheme();
+  return (
+    <Drawer variant="permanent" open={open}>
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'rtl' ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        <SidebarItem
+          open={open}
+          link="/home"
+          icon={<InboxIcon />}
+          text="Home"
+        />
+      </List>
+      <Divider />
+      <List>
+        <SidebarItem
+          open={open}
+          link="/admin"
+          icon={<AdminPanelSettingsIcon />}
+          text="Admin"
+        />
+      </List>
+    </Drawer>
+  );
+}
+
+export default Sidebar;
