26bb6d8d9b4afc1eac385a59e2da6abe7b57812a
Romain Tabary <tabaryr@gmail.com>
2022-10-21 17:41:40 +0200

    chore(api): add configuration

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '@nestjs/config';
+import { validate } from '../environments/env.validation';
+import environment from '../environments/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [environment],
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
